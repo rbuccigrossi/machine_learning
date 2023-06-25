e17ede58d287e5a8266dfb417552b5034c914900
Robert Buccigrossi <butch@tcg.com>
2023-06-25 17:16:20 -0400

    Added map-reduce over large documents

--- gradio_chatbot/grchatbot.py
+++ gradio_chatbot/grchatbot.py
@@ -59,12 +59,87 @@ def document_lookup_chat(history, search, top_n, prompt, model):
             "Answer the request based upon the above document sections. " +
             "If the answer is not clear from the source, state 'I cannot answer " +
             "based upon the documents provided.'\n\n " +
-            "Reqeust: " + prompt
+            "Request: " + prompt
         )
         # Now call the chatbot to get the response
         response = chatgpt.chat(chat_search, model)
         newhistory = history + [[prompt, response]]
         yield (newhistory, "", "")
+    except Exception as e:
+        newhistory = history + [[prompt, f"We received an error {str(type(e))}: {str(e)}"]]
+        print(traceback.format_exc())
+        yield (newhistory, None, None)
+        
+    
+def map_reduce_chat(history, document, prompt, model):
+    if (not(document) or not(prompt)):
+        newhistory = history + [[prompt, f"Please enter a document and a prompt"]]
+        yield (newhistory, None, None)
+        return
+    # Split the document into sections
+    # Split the text by newline
+    lines = document.split('\n')
+    
+    sections = []
+    section = []
+    count = 0
+    for line in lines:
+        line_word_count = len(line.split())
+        if count + line_word_count > 1500:
+            # Store entry and start new section
+            sections.append('\n'.join(section))
+            section = [line]
+            count = line_word_count
+        else:
+            # Add line to current section
+            section.append(line)
+            count += line_word_count
+    # Store last entry
+    sections.append('\n'.join(section))
+
+    try:
+        num_sections = len(sections)
+        n = 0
+        while (n < num_sections):
+            if (num_sections == 1): # There is only one section
+                chat_search = "The following is a document to be used for a request below\n\n---\n\n"
+                chat_search += sections[n] + "\n\n---\n\n"
+                chat_search += (
+                    "Answer the request based upon the above document section. " +
+                    "If the answer is not clear from the document, state 'I cannot answer " +
+                    "based upon the document provided.'\n\n " +
+                    "Request: " + prompt
+                )
+            elif (n < (num_sections - 1)): # Not the last section
+                chat_search = f"This is section {n+1} of {num_sections} of a document\n\n---\n\n"
+                chat_search += sections[n] + "\n\n---\n\n"
+                chat_search += (
+                    "Because the full document will not fit into the chat history size, please " +
+                    "answer the following request the best you can with what you have received " +
+                    "so far and provide details (such as quotes and summary information) " +
+                    "that you can then review and use as you receive subsequent sections. " +
+                    "If the answer is not clear from the document, state 'I cannot answer " +
+                    "based upon the document provided.'\n\n " +
+                    "Request: " + prompt
+                )
+            else: # The last section
+                chat_search = f"This is the last section of the document\n\n---\n\n"
+                chat_search += sections[n] + "\n\n---\n\n"
+                chat_search += (
+                    "Answer the request based upon your previous summary and the current section " +
+                    "If the answer is not clear from the document, state 'I cannot answer " +
+                    "based upon the document provided.'\n\n " +
+                    "Request: " + prompt
+                )
+            # Display the chat_search we are running
+            newhistory = history + [[chat_search, ""]]
+            yield (newhistory, None, None)
+            response = chatgpt.chat(chat_search, model)
+            # Display the response (and save it in our local copy of the history)
+            history = history + [[chat_search, response]]
+            yield (history, None, None)
+            # Move to the next section
+            n += 1
     except Exception as e:
         newhistory = history + [[prompt, f"We received an error {str(type(e))}: {str(e)}"]]
         print(traceback.format_exc())
@@ -104,8 +179,9 @@ with gr.Blocks(css="footer {visibility: hidden}", title="Chatbot Application") a
     with gr.Tab("Chat"):
         with gr.Row():
             with gr.Column(scale=100):
-                radio = gr.Radio(value='gpt-3.5-turbo', show_label=False, container=False,
-                                 choices=['gpt-3.5-turbo','gpt-3.5-turbo-16k','gpt-4'])
+                radio = gr.Radio(value='gpt-3.5-turbo', show_label=False,
+                                 choices=['gpt-3.5-turbo','gpt-3.5-turbo-16k','gpt-4']
+                ).style(container=False)
             with gr.Column(scale=2, min_width=110):
                 clear_chat_button = gr.Button("Clear Chat")
         chatbot = gr.Chatbot(value=[], elem_id="chatbot").style(height=500)
@@ -138,6 +214,19 @@ with gr.Blocks(css="footer {visibility: hidden}", title="Chatbot Application") a
                     ).style(container=False)
                 with gr.Column(scale=2, min_width=110):
                     dl_submit = gr.Button("Submit")
+        with gr.Tab("Question with Large Document"):
+            mr_document = gr.Textbox(
+                show_label=False, max_lines=5,
+                placeholder="Cut and Paste Document Here",
+            ).style(container=False)
+            with gr.Row():
+                with gr.Column(scale=100):
+                    mr_prompt = gr.Textbox(
+                        show_label=False,
+                        placeholder="Enter prompt and press enter",
+                    ).style(container=False)
+                with gr.Column(scale=2, min_width=110):
+                    mr_submit = gr.Button("Submit")
     with gr.Tab("Library"):
         with gr.Row(variant="panel"):
             with gr.Column():
@@ -160,7 +249,11 @@ with gr.Blocks(css="footer {visibility: hidden}", title="Chatbot Application") a
                     [chatbot, dl_search, dl_prompt])
     dl_submit.click(document_lookup_chat, [chatbot, dl_search, dl_top_n, dl_prompt, radio],
                     [chatbot, dl_search, dl_prompt])
-    # Document Lookup Chat
+    # Question with Large Document (Map-Reduce)
+    mr_prompt.submit(map_reduce_chat, [chatbot, mr_document, mr_prompt, radio],
+                    [chatbot, mr_document, mr_prompt])
+    mr_submit.click(map_reduce_chat, [chatbot, mr_document, mr_prompt, radio],
+                    [chatbot, mr_document, mr_prompt])
     # LIBRARY DIALOG
     lib_delete_button.click(remove_document, [lib_doc_list], [lib_doc_list, lib_status])
     lib_upload.upload(add_document, [lib_upload], [lib_doc_list, lib_status])
